b5b4c8f7b4d4331fcfc362c9b7f133080184d0bf
Harsh Singh <harsh945455@gmail.com>
2025-04-16 21:55:46 +0530

    Time: 5 ms (46.79%), Space: 26.5 MB (43.99%) - LeetHub

--- 0680-valid-palindrome-ii/0680-valid-palindrome-ii.cpp
+++ 0680-valid-palindrome-ii/0680-valid-palindrome-ii.cpp
@@ -1,33 +1,6 @@
-// class Solution {
-// public:
-//     bool validPalindrome(string s) {
-//         int cnt=0;
-//         int i=0;
-//         int j=s.length()-1;
-//         while(i<=j){
-//             if(s[i]==s[j]){
-//                 i++;
-//                 j--;
-//             }
-//             else{
-//                 cnt++;
-//                 i++;
-//             }
-        
-//             if(cnt>1){
-//                 return false;
-//             }
-//         }
-//         if(cnt==0 || cnt==1){
-//             return true;
-//         }
-//      return false;
-        
-//     }
-// };
 class Solution {
 public:
-    bool isPalindrome(string s, int i, int j) {
+  bool isPalindrome(string s, int i, int j) {
         while (i <= j) {
             if (s[i] != s[j]) return false;
             i++;
@@ -39,13 +12,11 @@ public:
     bool validPalindrome(string s) {
         int i = 0;
         int j = s.length() - 1;
-
         while (i <= j) {
             if (s[i] == s[j]) {
                 i++;
                 j--;
             } else {
-                // Try skipping either the left or the right character
                 return isPalindrome(s, i + 1, j) || isPalindrome(s, i, j - 1);
             }
         }
