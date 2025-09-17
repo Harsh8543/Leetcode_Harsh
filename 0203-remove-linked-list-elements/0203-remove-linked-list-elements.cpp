2f3f50d5a7ae80bc09c015bad47e123821d19d5c
Harsh Singh <harsh945455@gmail.com>
2025-09-17 23:42:08 +0530

    Time: 0 ms (100%), Space: 20 MB (74.66%) - LeetHub

--- 0203-remove-linked-list-elements/0203-remove-linked-list-elements.cpp
+++ 0203-remove-linked-list-elements/0203-remove-linked-list-elements.cpp
@@ -1,45 +1,47 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
 class Solution {
 public:
     ListNode* removeElements(ListNode* head, int val) {
-        // Step 1: Remove leading nodes with value == val
-        while (head != nullptr && head->val == val) {
-            ListNode* temp = head;
-            head = head->next;
-            delete temp;
-        }
+//         ListNode*temp=head;
+//         // ListNode*ptr=temp->next;
+// while(temp!=NULL){
+//     if(curr->val==val){
+//         temp->next=curr->next;
+//         curr=temp->next;
+//     }
+//     else{
+//         temp=curr;
+//         curr=curr->next;
+//     }
+// }
+// return head;
 
-        // Step 2: Traverse the rest of the list
-        ListNode* curr = head;
-        while (curr != nullptr && curr->next != nullptr) {
-            if (curr->next->val == val) {
-                ListNode* temp = curr->next;   // node to delete
-                curr->next = temp->next;       // bypass node
-                delete temp;                   // free memory
+if (head == NULL) return head;  
+        while (head != NULL && head->val == val) {
+            head = head->next;
+        }
+        if (head == NULL) return head;
+        ListNode* temp = head;
+        ListNode* curr = temp->next;
+
+        while (temp != NULL && curr != NULL) {
+            if (curr->val == val) {
+                temp->next = curr->next;  
+                curr = temp->next;        
             } else {
-                curr = curr->next;             // move ahead
+                temp = curr;             
+                curr = curr->next;
             }
         }
-
         return head;
-
-
-    //  while (head != nullptr && head->val == val) {
-    //         ListNode* temp = head;
-    //         head = head->next;
-    //         delete temp;
-    //     }
-
-    //     ListNode* curr = head;
-    //     while (curr != nullptr && curr->next != nullptr) {
-    //         if (curr->next->val == val) {
-    //             ListNode* temp = curr->next;
-    //             curr->next = curr->next->next;
-    //             delete temp;
-    //         } else {
-    //             curr = curr->next;
-    //         }
-    //     }
-
-    //     return head;
     }
 };
