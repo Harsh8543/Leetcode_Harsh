19b11d52901da921d0eb778a363d5a2ccf967a4b
Harsh Singh <harsh945455@gmail.com>
2025-09-17 23:03:32 +0530

    Time: 0 ms (100%), Space: 14.9 MB (91.52%) - LeetHub

--- 0019-remove-nth-node-from-end-of-list/0019-remove-nth-node-from-end-of-list.cpp
+++ 0019-remove-nth-node-from-end-of-list/0019-remove-nth-node-from-end-of-list.cpp
@@ -8,25 +8,60 @@
  *     ListNode(int x, ListNode *next) : val(x), next(next) {}
  * };
  */
+// class Solution {
+//     int height(ListNode*head){
+//         int k=0;
+//         ListNode*temp1=head;
+//         while(temp1!=NULL){
+//             temp1=temp1->next;
+//             k++;
+//         }
+//         return k;
+//     }
+// public:
+//     ListNode* removeNthFromEnd(ListNode* head, int n) {
+//       ListNode* temp=head;
+//       int cnt=0;
+//         int l=k-n;
+//       while(temp!=NULL){
+//         temp=temp->next;
+//         cnt++;
+//         if(cnt==l){
+//           temp->next=temp->next->next;
+//         }
+//       }
+//       return head;
+//   }
+// };
+
 class Solution {
+    int height(ListNode* head) {
+        int k = 0;
+        ListNode* temp1 = head;
+        while (temp1 != NULL) {
+            temp1 = temp1->next;
+            k++;
+        }
+        return k;
+    }
 public:
     ListNode* removeNthFromEnd(ListNode* head, int n) {
-          ListNode* dummy = new ListNode(0, head);
-        ListNode* fast = dummy;
-        ListNode* slow = dummy;
-
-        for(int i = 0; i <= n; i++) {
-            fast = fast->next;
+        int k = height(head);  
+        int l = k - n;
+        if (l == 0) {
+            return head->next;
         }
 
-        while(fast != nullptr) {
-            fast = fast->next;
-            slow = slow->next;
+        ListNode* temp = head;
+        int cnt = 0;
+        while (temp != NULL) {
+            cnt++;
+            if (cnt == l) {  
+                temp->next = temp->next->next; 
+                break; 
+            }
+            temp = temp->next;
         }
-
-        // Now slow->next is the node to remove
-        slow->next = slow->next->next;
-
-        return dummy->next; 
+        return head;
     }
 };
