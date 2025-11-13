59fe62d9ce9b59cdf4bbf164f8cbbcaae555f6cf
Harsh Singh <harsh945455@gmail.com>
2025-11-13 11:28:32 +0530

    Time: 0 ms (100%), Space: 15 MB (40.03%) - LeetHub

--- 0199-binary-tree-right-side-view/0199-binary-tree-right-side-view.cpp
+++ 0199-binary-tree-right-side-view/0199-binary-tree-right-side-view.cpp
@@ -9,23 +9,105 @@
  *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
  * };
  */
+// class Solution {
+// public:
+//     vector<int> rightSideView(TreeNode* root) {
+//         vector<int>har;
+//         if(!root) return har;
+//         RView(root,0,har);
+//         return har;
+
+//     }
+//     private:
+//     void RView(TreeNode* root,int level, vector<int>&har){
+//         if(!root) return;
+//         if(level==har.size()){
+//             har.push_back(root->val);
+//         }
+//         RView(root->right,level+1,har);
+//         RView(root->left,level+1,har);
+       
+//     }
+// };
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 class Solution {
 public:
     vector<int> rightSideView(TreeNode* root) {
-        vector<int>har;
-        if(!root) return har;
-        RView(root,0,har);
-        return har;
-
-    }
-    private:
-    void RView(TreeNode* root,int level, vector<int>&har){
-        if(!root) return;
-        if(level==har.size()){
-            har.push_back(root->val);
+        vector<int> har;
+        if (!root) return har;
+        queue<TreeNode*> q;
+        q.push(root);
+        while (!q.empty()) {
+            int n = q.size();
+            har.push_back(q.back()->val);
+            while (n--) {
+                TreeNode* temp = q.front();
+                q.pop();
+                if (temp->left)
+                    q.push(temp->left);
+                if (temp->right)
+                    q.push(temp->right);
+            }
         }
-        RView(root->right,level+1,har);
-        RView(root->left,level+1,har);
-       
+        return har;
     }
 };
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
